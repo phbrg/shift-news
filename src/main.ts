eb3a7d9d3aebdcb85133e85615e05cd88c21eadb
phbrg <xomaicrocefalia123@gmail.com>
2024-03-26 22:21:11 -0300

    added request limiter to prevent brute force/ddos

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import rateLimit from 'express-rate-limit';
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  keyGenerator: function(req, res) {
+    return req.ip || req.connection.remoteAddress;
+  }
+});
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(limiter);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
     origin: 'http://localhost:3000',
--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -3,13 +3,13 @@ import { PostService } from "./post.service";
 import { AuthGuard } from "src/guards/auth.guard";
 import { CreatePostDTO } from "./dto/create-post.dto";
 
-@UseGuards(AuthGuard)
 @Controller('post')
 export class PostController {
   constructor(
     private readonly postService: PostService
   ) {}
-
+  
+  @UseGuards(AuthGuard)
   @Post()
   async createPost(@Body() body: CreatePostDTO, @Req() req: Request) {
     return this.postService.createPost(body, req);
@@ -20,16 +20,19 @@ export class PostController {
     return this.postService.getPosts(param);
   }
 
+  @UseGuards(AuthGuard)
   @Put(':id')
   async editPost(@Param() param: { id: string }, @Body() body: any, @Req() req: Request) {
     return this.postService.editPost(param.id, body, req);
   }
 
+  @UseGuards(AuthGuard)
   @Delete(':id')
   async deletePost(@Param() param: { id: string }, @Req() req: Request) {
     return this.postService.deletePost(param.id, req);
   }
 
+  @UseGuards(AuthGuard)
   @Post('up/:id')
   async upPost(@Param() param: { id: string }, @Req() req: Request) {
     return this.postService.upPost(param.id, req);
